0b6fd2df620135486d9f10c48663c85cc95f0ac3
Timcso <timea.jakabfy@spicyanalytics.com>
2016-04-07 12:11:50 +0200

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
-  end
-
-  def hello
-  	render text: "Hello!"
   end
 
   def about
